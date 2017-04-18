d619bd5d3381db4c668f5d28f4d9aefc60e867f1
yanchevskaya <aliceyanchevskaya@gmail.com>
2017-04-18 10:21:27 +0300

    using testng

---
+++ src/main/java/shape/Square.java
@@ -0,0 +1,14 @@
+package shape;
+
+public class Square {
+  private int l;
+
+  public Square(int l) {
+    this.l = l;
+  }
+
+  public int area(){
+    return l*l;
+  }
+
+}
---
+++ src/test/java/shape/SquareTest.java
@@ -0,0 +1,13 @@
+package shape;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SquareTest {
+
+  @Test
+  public void testArea(){
+    Square s = new Square(5);
+    Assert.assertEquals(s.area(), 25);
+  }
+}
